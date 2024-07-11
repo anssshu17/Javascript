71315037294ed1f5dd0fccc5e8856b327611af18
Anshu Raj <112803091+anssshu17@users.noreply.github.com>
2024-07-11 12:14:54 +0000

    conversion of data types

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,25 @@
+let score = '33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn =1
+
+let booleanIsLoggedIN= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIN);
+
+//1 => true; 0=> false
+//"" => false
+// "anshu" => true
+
+let someNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
